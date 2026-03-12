220f9e34ba76fd10972b62c47adea65bcede1c4c
Hafiz Muhammad Tayyab <hmtayyab76@gmail.com>
2026-03-12 18:54:20 +0500

    Updated Brista code

--- assets/theme.js
+++ assets/theme.js
@@ -9031,6 +9031,12 @@ register("header", {
         (_this$accountQuickVie = _this.accountQuickView) === null || _this$accountQuickVie === void 0 ? void 0 : _this$accountQuickVie.unload();
         _this.accountQuickView = AccountQuickView(n$2(selectors$E.accountQuickView));
       });
+    }), e$2(document, "apps:product-added-to-cart", function () {
+      cart.get().then(function (data) {
+        r$1("cart:updated", {
+          cart: data
+        });
+      });
     }), e$2(menuButtons, "click", function (event) {
       event.preventDefault();
       r$1("mobile-menu:open");
@@ -9776,7 +9782,7 @@ var selectors$v = {
   subtotal: ".quick-cart__footer-subtotal span",
   quantityInput: ".quick-cart .quantity-input__input",
   quantityItem: "[data-input-item]",
-  discounts: ".quick-cart__item-discounts",
+  cartDiscounts: ".quick-cart__footer .quick-cart__item-discounts",
   freeShippingBar: "[data-free-shipping-bar]",
   crossSells: "[data-cross-sells]"
 };
@@ -9811,6 +9817,9 @@ register("quick-cart", {
     }), c("quantity-update:remove", function (_, _ref3) {
       var key = _ref3.key;
       _this.handleItemRemoval(key);
+    }), e$2(document, "apps:product-added-to-cart", function () {
+      _this.refreshQuickCart();
+      r$1("quick-cart:open");
     })];
     this.quantityButtons = QuantityButtons(this.container);
     this.cartNoteToggle = CartNoteToggle(this.container);
@@ -9886,8 +9895,8 @@ register("quick-cart", {
         // Render subtotal
         updateInnerHTML("".concat(selectors$v.cartWrapper, " ").concat(selectors$v.subtotal), container);
 
-        // Render promotions
-        updateInnerHTML("".concat(selectors$v.cartWrapper, " ").concat(selectors$v.discounts), container);
+        // Render cart-level discounts
+        updateInnerHTML("".concat(selectors$v.cartWrapper, " ").concat(selectors$v.cartDiscounts), container);
 
         // Render cross sells
         if (crossSells) {
@@ -13716,7 +13725,7 @@ c("cart:updated", function (responseData) {
 
 // Make it easy to see exactly what theme version
 // this is by commit SHA
-window.SHA = "c08d0ad035";
+window.SHA = "809551887c";
 if (!sessionStorage.getItem("flu_stat_recorded") && !((_window$Shopify = window.Shopify) !== null && _window$Shopify !== void 0 && _window$Shopify.designMode)) {
   var _window$Shopify2, _window$Shopify3;
   // eslint-disable-next-line no-process-env
